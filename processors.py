3d737ff1027714630e74bc9e5826f4d041d63538
alanrproject <alanrproject@outlook.com>
2024-06-30 20:05:14 -0500

    Hasta aquí se guarda el nuevo texto en un archivo nuevo pero sin estilo

--- processors.py
+++ processors.py
@@ -3,6 +3,7 @@ from wordreader import WordReader
 from memoryreader import MemoryReader
 from wordmemory import WordMemory
 from pngmemory import PngMemory
+from docx import Document
 
 class DocumentProcessor:
     def __init__(self, word_path, memory_sheets, output_file):
@@ -12,7 +13,7 @@ class DocumentProcessor:
         self.png_memory = PngMemory()
 
     def process_document(self):
-        #Leer documento word base
+        # Leer documento word base
         text, headings, images = self.word_reader.read()
         print("Text:", text)
         print("Headings:", headings)
@@ -20,6 +21,21 @@ class DocumentProcessor:
         
         df = pd.DataFrame(self.memory_reader.read())
         print(df)
+        
+        # Reemplazar valores en el texto
+        for _, row in df.iterrows():
+            variable_name = row['Nombre de la variable']
+            value = row['Valor']
+            text = text.replace(variable_name, str(value))
+        
+        print("Updated Text:", text)
+
+        # Guardar el texto actualizado en un nuevo archivo DOCX
+        output_docx = 'PE1126_updated.docx'
+        doc = Document()
+        doc.add_paragraph(text)  # Agrega el texto actualizado como un párrafo en el documento
+        doc.save(output_docx)
+        print(f"Archivo '{output_docx}' generado con éxito.")
 
 
 
