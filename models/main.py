ce1011c37880c8a1149d2611237fd7d715f49c2f
alanrproject <alanrproject@outlook.com>
2024-07-03 16:48:34 -0500

    Actualización diccionario imágenes

--- models/main.py
+++ models/main.py
@@ -2,9 +2,12 @@ from processors import DocumentProcessor
 
 def main():
     word_path = 'raw/PE1126_Memorias de cálculo.docx'
+    # Diccionario de las tablas que se van a incrustar en la memoria
     sheetnames = {'DimensionSistema':'TablaDimensionSistema',
                       'DistCadenas':'TablaDistCadenas'}
-    image_dict = {'Imagen 1: Vista aérea':'raw/vistaaerea.png'}  # Specify the output Excel file path
+    # Diccionario de las imágenes que se van a incrustar en la memoria
+    image_dict = {'Imagen 1: Vista aérea':'raw/vistaaerea.png',
+                  'Imagen 2: Temperatura Mensual':'raw/Temperaturamensual.png'}
     
     processor = DocumentProcessor(word_path, sheetnames, image_dict)
     processor.process_document()
